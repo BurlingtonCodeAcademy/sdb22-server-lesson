ed946b365ccc2e49ed559ac63cb6a73094c999f8
Paul Niemczyk <mrpndev@gmail.com>
2022-07-13 15:39:29 -0400

    API; HTTP request/response; REST; Endpoints; Controllers; MVC; middlewares

---
+++ app.js
@@ -0,0 +1,41 @@
+const Express = require("express")
+const app = Express()
+
+const mainRoutes = require("./controllers/mainRoutes")
+
+const PORT = 4000
+
+/* 
+    Express handles server process and route management (thru controllers)
+    
+    To use:
+        * Import express
+        * set app variable to top level Express function invokation
+        * setup your app.listen method to listen on a port.
+            * takes port as first param
+            * takes anonymous no param callback to run.
+        * use app.http-method to handle each route
+            * takes string of route as first param
+            * takes handler callback that contains logic
+                * handler has request and response parameters
+                * req and res parameters have access to express methods!!!
+
+*/
+
+app.use(Express.static(__dirname + "/static"))
+
+const logTime = (req, res, next) => {
+    let date = new Date()
+    console.log(date.toLocaleDateString())
+    next()
+}
+
+// Express.json() parses our json request object.
+app.use(Express.json())
+app.use(Express.urlencoded())
+app.use(logTime)
+app.use(mainRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
---
+++ controllers/mainRoutes.js
@@ -0,0 +1,64 @@
+const router = require("express").Router()
+
+router.get("/", (_, res) => {
+    res.render("index")
+})
+
+router.get("/all", (req, res) => {
+    res.status(200).json({
+        status: "Getting all items"
+    })
+})
+
+router.get("/form", (req, res) => {
+    const queries = req.query
+    console.log(queries)
+    res.status(200).json({
+        status: "Success",
+        queries
+    })
+})
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params
+    
+    res.status(200).json({
+        status: `Success`,
+        id
+    })
+})
+
+
+router.post("/login", (req, res) => {
+
+    const { username, password } = req.body
+    
+    res.status(200).json({
+        status: "User logged in.",
+        username,
+        password
+    })
+})
+
+router.post("/register", (req, res) => {
+    
+    const { name, surname, email, password } = req.body
+    console.log(req.body)
+    try {
+        if (!name || !surname || !email || !password) {
+            res.status(406).json({
+                status: "Failed. Insufficient data."
+            })
+        } else {
+            res.status(200).json({
+                status: "User registered",
+            })
+        }
+    } catch(error) {
+        res.status(500).json({
+            status: `${error}`
+        })
+    }
+})
+
+module.exports = router
